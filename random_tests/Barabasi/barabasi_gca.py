7375c655b6616f9c3665ce974b18f26e460f5d57
Enes Sefa Ayar <92874184+EnesSefaAyar@users.noreply.github.com>
2023-04-01 15:15:43 +0300

    Update barabasi_gca.py

--- random_tests/Barabasi/barabasi_gca.py
+++ random_tests/Barabasi/barabasi_gca.py
@@ -1,5 +1,5 @@
 from joblib import Parallel, delayed
 import main_barabasi as m
 
-res = Parallel(n_jobs=1)(delayed(m.main)(i, (i+1)*200) for i in range(0, 100))
+res = Parallel(n_jobs=-1)(delayed(m.main)(i, (i+1)*200) for i in range(0, 100))
 
